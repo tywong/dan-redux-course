b4ac66fe3a05d5a8cb2dd3f053ad827eb8ef19c8
Mole Wong <tszyeung@gmail.com>
2016-12-28 00:43:09 +0800

    implementing middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,35 +1,33 @@
 import { createStore } from 'redux';
 import todoAppReducer from './reducers/index';
 
-const addLoggingToDispatch = (store) => {
-    const rawDispatch = store.dispatch;
+const logger = (store) => (next) => (action) => {
+    const nextState = next(action);
 
-    return (action) => {
-        const nextState = rawDispatch(action);
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    console.log('%c next state', 'color: green', nextState);
+    console.groupEnd(action.type);
 
-        console.group(action.type);
-        console.log('%c prev state', 'color: gray', store.getState());
-        console.log('%c action', 'color: blue', action);
-        console.log('%c next state', 'color: green', nextState);
-        console.groupEnd(action.type);
+    return nextState;
+};
 
-        return nextState;
+const promise = (store) => (next) => (action) => {
+    if(typeof action.then === 'function') {
+        return action.then( next );
     }
-}
+    else {
+        return next(action);
+    }
+};
 
-const addPromiseSuppportToDispatch = (store) => {
-    const rawDispatch = store.dispatch;
-
-    return (action) => {
-        if(typeof action.then === 'function') {
-            return action.then( rawDispatch );
-        }
-        else {
-            console.log(2);
-            return rawDispatch(action);
-        }
-    };
-}
+const middlewares = [promise, logger];
+const wrapDisptachWithMiddlewares = (store, middlewares) => {
+    middlewares.slice().reverse().forEach( (middleware) => {
+        store.dispatch = middleware(store)(store.dispatch)
+    });
+};
 
 const configureStore = () => {
     const store = createStore(
@@ -37,9 +35,7 @@ const configureStore = () => {
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
 
-    store.dispatch = addLoggingToDispatch(store);
-    store.dispatch = addPromiseSuppportToDispatch(store);
-
+    wrapDisptachWithMiddlewares(store, middlewares);
     return store;
 };
 
