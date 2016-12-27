3d798d392c99646eb1662b4a09c552b0193c68cc
Mole Wong <tszyeung@gmail.com>
2016-12-27 17:58:06 +0800

    updated FilterLink

---
+++ src/FilterLink.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const FilterLink = ( {filter, children} )  => (
+    <Link to={ filter === 'all' ? '' : filter } >
+        {children}
+    </Link>
+);
+
+export default FilterLink;
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,54 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
-
-import { filterLinkClick } from './actions';
-
-
-const Link = ({
-    active,
-    onFilterClick,
-    children
-} ) => {
-    if(active) {
-        return (
-            <span>{ children }</span>
-        );
-    }
-
-    return (
-        <a href='#'
-            onClick={ (e) => {
-                e.preventDefault();
-                onFilterClick();
-            } }
-        > { children } </a>
-    );
-}
-
-const mapStateToProps = (state, ownProps) => ({
-    active: state.visibilityFilter === ownProps.filter
-});
-
-const mapDispatchToProps = (dispatch, ownProps) => ({
-    onFilterClick: () => {
-        dispatch(filterLinkClick(ownProps.filter))
-    }
-});
-
-const FilterLink = connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Link);
+import FilterLink from './FilterLink.jsx';
 
 const Footer = () => (
     <p>
         Show:
         { ' ' }
-        <FilterLink filter={ 'SHOW_ALL' }>All</FilterLink>
+        <FilterLink filter={ 'all' }>All</FilterLink>
         { ', ' }
-        <FilterLink filter={ 'SHOW_ACTIVE' }>Active</FilterLink>
+        <FilterLink filter={ 'active' }>Active</FilterLink>
         { ', ' }
-        <FilterLink filter={ 'SHOW_COMPLETED' }>Completed</FilterLink>
+        <FilterLink filter={ 'completed' }>Completed</FilterLink>
     </p>
 );
 
