f2e416a82b20361667eca10ec6b045773e8055ce
Mole Wong <tszyeung@gmail.com>
2016-12-27 22:30:57 +0800

    change todos reducer; add log to dispatch function

--- src/configureStore.js
+++ src/configureStore.js
@@ -4,6 +4,21 @@ import todoAppReducer from './reducers/index';
 import { loadState, saveState } from './localState';
 import throttle from 'lodash/throttle';
 
+const addLoggingToDispatch = (store) => {
+    const rawDispatch = store.dispatch;
+
+    return (action) => {
+        const nextState = rawDispatch(action);
+
+        console.group(action.type);
+        console.log('%c prev state', 'color: gray', store.getState());
+        console.log('%c action', 'color: blue', action);
+        console.log('%c next state', 'color: green', nextState);
+        console.groupEnd(action.type);
+
+        return nextState;
+    }
+}
 
 const configureStore = () => {
     const store = createStore(
@@ -12,6 +27,7 @@ const configureStore = () => {
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
 
+    store.dispatch = addLoggingToDispatch(store);
 
     store.subscribe(
         throttle( () => {
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,28 +1,45 @@
 import todo from './todo';
+import { combineReducers } from 'redux';
 
-const todos = (state = [], action) => {
+const byId = (state = {}, action) => {
     switch(action.type) {
         case 'ADD_TODO':
-        return [
-            ...state,
-            todo( action.todo, action )
-        ];
         case 'TOGGLE_TODO':
-        return state.map( (t) => todo(t, action) );
+        return {
+            ...state,
+            [action.id]: todo( state[action.id], action )
+        };
         default:
         return state;
     }
 };
 
-export default todos;
-
-export const getVisibleTodos = (state, filter) => {
-    switch(filter) {
-        case 'active':
-        return state.filter( t => !t.completed );
-        case 'completed':
-        return state.filter( t => t.completed );
+const allIds = (state = [], action) => {
+    switch(action.type) {
+        case 'ADD_TODO':
+        return [...state, action.id];
         default:
         return state;
+    }
+};
+
+const todos = combineReducers({
+    byId,
+    allIds
+});
+
+export default todos;
+
+const getAllTodos = (state) => state.allIds.map( id => state.byId[id] );
+
+export const getVisibleTodos = (state, filter) => {
+    const allTodos = getAllTodos(state);
+    switch(filter) {
+        case 'active':
+        return allTodos.filter( t => !t.completed );
+        case 'completed':
+        return allTodos.filter( t => t.completed );
+        default:
+        return allTodos;
     }
 }
